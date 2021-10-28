5c891a5b0670e2fc411696c37a4f58db3c26160b
djayappiah <djappiah@st.ug.edu.gh>
2021-10-28 11:23:43 +0000

    Server bootstrapped  successfully

---
+++ index.js
@@ -0,0 +1,46 @@
+const {ApolloServer, gql} = require("apollo-server")
+
+const books =[
+{   title:"Harry Potter",
+    author:"Djayy",
+    ISBN:"123-456-8",
+},
+{   title:"Angels&Demons",
+    author:"Djayy",
+    ISBN:"123-456-8",
+},
+]
+
+const schemas = gql`
+type Book {
+    title:String!
+    author:String!
+    ISBN:String,
+}
+type Query {
+    books:[Book]
+    book(title: String!): Book
+}
+type Mutation{
+    createBook(title:String!, author:String!, ISBN:String):Book
+}`
+
+
+const booksResolvers = {
+    Query: {
+        books: () => books,
+        book:(parent,args) => books.find(book => book.title == args.title)
+    },
+
+    Mutation: {
+        createBook: (parent, args) => {
+            const {title, author, ISBN} = args;
+            const book = {title, author, ISBN};
+            books.push(book);
+            return book;
+        }
+    },
+}
+
+const server = new ApolloServer({typeDefs: schemas, resolvers:booksResolvers})
+server.listen(8000).then(({url, port}) => {console.log(`Server ready at ${url} and ready to be used`);}).catch (err => console.log(err.message))
